f1ae435542fbd27a2bbc949e644af5afff6112c3
Natalya <natasha-vd@yandex.ru>
2023-12-17 17:28:25 +0300

    add subCollection in firestore

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -20,11 +20,22 @@ export class CategoriesComponent {
     const data: Category = {
       category: this.categoryForm.value.category
     }
+    const subCategoryData = {
+      subcategory: 'subcategory1'
+    }
     this.firestore
       .collection<Category>('categories')
       .add(data)
       .then(docRef => {
         console.log(docRef)
+        this.firestore
+          .collection('categories')
+          .doc(docRef.id)
+          .collection('subcategories')
+          .add(subCategoryData)
+          .then(subDocRef => {
+            console.log(subDocRef)
+          })
       })
       .catch(err => {
         console.log(err)
