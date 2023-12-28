391985ddf8048570fed94e59625638d6f8f71191
natalya <natasha-vd@yandex.ru>
2023-12-28 20:49:10 +0300

    add select file & load categories

--- src/app/posts/new-post/new-post.component.spec.ts
+++ src/app/posts/new-post/new-post.component.spec.ts
@@ -2,29 +2,65 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewPostComponent } from './new-post.component';
 import { FormsModule } from '@angular/forms';
-import { findEl, setFieldValue } from 'src/app/spec-helpers/element.spec-helper';
+import { findEl, findEls, setFieldValue } from 'src/app/spec-helpers/element.spec-helper';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { CategoryFromFirebase } from 'src/app/models/category';
+import { of } from 'rxjs';
 
 describe('NewPostComponent', () => {
   let component: NewPostComponent;
   let fixture: ComponentFixture<NewPostComponent>;
+  let categoryServiceSpy: jasmine.SpyObj<CategoryService>
+  let categoriesData: CategoryFromFirebase[] = []
 
   beforeEach(() => {
+    categoriesData = [
+      {
+        id: '1',
+        data: {category: 'Category 1'}
+      },
+      {
+        id: '2',
+        data: {category: 'Category 2'}
+      },
+      {
+        id: '3',
+        data: {category: 'Category 3'}
+      },
+    ]
+
+    const categoryServiceSpyObj = jasmine.createSpyObj('CategoryService', ['getCategoriesList'])
+
     TestBed
     .configureTestingModule({
       declarations: [NewPostComponent],
       imports: [FormsModule],
+      providers: [
+        {
+          provide: CategoryService,
+          useValue: categoryServiceSpyObj
+        }
+      ]
     })
     .compileComponents();
+
+    categoryServiceSpy = TestBed.inject(CategoryService) as jasmine.SpyObj<CategoryService>
 
     fixture = TestBed.createComponent(NewPostComponent);
     component = fixture.componentInstance;
 
+    categoryServiceSpy.getCategoriesList.and.returnValue(of(categoriesData));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add data in categories component', () => {
+    expect(component.categories).toEqual(categoriesData);
+    expect(categoryServiceSpy.getCategoriesList).toHaveBeenCalledTimes(1);
+  })
 
   describe('onTitleChange()', () => {
     it('should write the text in variable component', () => {
@@ -38,11 +74,55 @@ describe('NewPostComponent', () => {
     })
   });
 
+  describe('showPreview()', () => {
+    it('should select file', () => {
+      const mockFile = new File([''], 'filename', { type: 'jpg' });
+      const mockEvt = { target: { files: [mockFile] } } as unknown as Event;
+      const mockReader: FileReader = jasmine.createSpyObj('FileReader', ['readAsDataURL', 'onload']);
+      spyOn(window, 'FileReader').and.returnValue(mockReader);
+
+      component.showPreview(mockEvt);
+
+      expect(window.FileReader).toHaveBeenCalled();
+      expect(mockReader.readAsDataURL).toHaveBeenCalledOnceWith(mockFile); // Проходит даже если отправить сюда новый new File(), хотя не должен
+      expect(component.selectedImg).toEqual(mockFile);
+    })
+  })
+
   describe('HTML template', () => {
     it('should disabled input permalink', () => {
       const element = findEl(fixture, 'permalink').nativeElement as HTMLInputElement
 
       expect(element.disabled).toBe(true);
+    })
+
+    it('should call method showPreview', () => {
+      const showPreviewSpy = spyOn(component, 'showPreview');
+      const element = findEl(fixture, 'image-preview')
+
+      element.triggerEventHandler('change', {});
+
+      expect(showPreviewSpy).toHaveBeenCalled();
+    })
+
+    it('should accept attribute in input type=file', () => {
+      const element = findEl(fixture, 'image-preview').nativeElement as HTMLInputElement;
+
+      expect(element.accept).toBe('image/*');
+    })
+
+    it('should render categories in option', () => {
+      const elements = findEls(fixture, 'option-category');
+
+      for(let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const option = element.nativeElement as HTMLOptionElement;
+
+        expect(option.value).toBe(categoriesData[i].id);
+        expect(option.textContent).toContain(categoriesData[i].data.category);
+      }
+
+      expect(elements.length).toBe(categoriesData.length);
     })
   })
 });
--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryFromFirebase } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
   permalink: string = '';
+  imgSrc: any = './assets/placeholder-image.png';
+  selectedImg: any;
+  categories: CategoryFromFirebase[] = [];
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategoriesList().subscribe(val => {
+      this.categories = val
+    })
+  }
 
   onTitleChange($event: Event) {
     const input = $event.target as HTMLInputElement
     const title = input.value
     this.permalink = title.replace(/\s/g, '-')
+  }
+
+  showPreview($event: Event) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result;
+    }
+
+    const input = $event.target as HTMLInputElement
+
+    if(input.files) {
+      reader.readAsDataURL(input.files[0])
+      this.selectedImg = input.files[0]
+    }
   }
 }
